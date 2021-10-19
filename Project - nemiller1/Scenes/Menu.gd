098aba2d16230fa3539a940d3c0db00f032eeb1f
ThunderBuddy01 <38160543+Thunderbuddy01@users.noreply.github.com>
2021-10-19 16:44:04 -0400

    10-19-21 Update 2

--- Project - nemiller1/Scenes/Menu.gd
+++ Project - nemiller1/Scenes/Menu.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Level 1.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Scenes/Artist Statement.tscn")
